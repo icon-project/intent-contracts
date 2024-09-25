373f8d95be870b7480ffdbc314311ae721d56f94
ibrizsabin <sabin.chitrakar@ibriz.ai>
2024-09-25 09:41:46 +0545

    fix: remove unwanted files

--- contracts/cosmwasm-vm/cw-xcall-lib/src/dapp_msg.rs
+++
@@ -1,8 +0,0 @@
-use cosmwasm_schema::cw_serde;
-
-use crate::network_address::NetworkAddress;
-
-#[cw_serde]
-pub enum ExecuteMsg {
-    HandleCallMessage { from: NetworkAddress, data: Vec<u8> },
-}
--- contracts/cosmwasm-vm/cw-xcall-lib/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod dapp_msg;
-pub mod dapp_multi_msg;
-pub mod message;
-pub mod network_address;
-pub mod xcall_connection_msg;
-pub mod xcall_msg;
