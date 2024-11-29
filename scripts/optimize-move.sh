b5164a2d3f30069101b716a3a68ebab3f56bf7c4
ibrizsabin <sabin.chitrakar@ibriz.ai>
2024-11-29 18:39:39 +0545

    fix: move build script

--- scripts/optimize-move.sh
+++ scripts/optimize-move.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # contracts
-CONTRACTS=("contracts/sui/intent_v1" "contracts/sui/libs/sui_rlp" )
+CONTRACTS=("contracts/sui/intent_v1")
 
 #cargo install --locked --git https://github.com/MystenLabs/sui.git --branch testnet sui
 start_dir=$(pwd)
