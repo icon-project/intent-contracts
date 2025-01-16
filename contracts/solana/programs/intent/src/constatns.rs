c14f90688753a5cdfecef53694e509bd4f0c4a68
bishalbikram <contact.bishalbikram@gmail.com>
2025-01-16 11:10:55 +0545

    chore: remove duplicate file

--- contracts/solana/programs/intent/src/constatns.rs
+++
@@ -1,5 +0,0 @@
-pub const ACCOUNT_DISCRIMINATOR_SIZE: usize = 8;
-
-pub const NATIVE_ADDRESS: &str = "11111111111111111111111111111111";
-
-pub const VAULT_TOKEN_SEED_PREFIX: &'static str = "vault_token";
